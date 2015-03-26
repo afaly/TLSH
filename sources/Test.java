1ed2a216a454e82258e401db3a68c7c7448d848e
Nuno Brito <nuno.brito@triplecheck.de>
2015-03-26 11:57:32 +0100

    Adding test case

--- sources/Test.java
+++ sources/Test.java
@@ -1,18 +1,17 @@
 
+import java.io.DataInputStream;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.File;
 
 /*
  * SPDXVersion: SPDX-1.1
- * Creator: Person: Vasiliy Vadimov
- * Creator: Person: Nuno Brito
+  Creator: Person: Nuno Brito
  * Creator: Organization: TripleCheck (contact@triplecheck.de)
  * Created: 2015-01-08T01:26:56Z
  * LicenseName: Apache-2.0
  * FileName: Test.java  
- * FileCopyrightText: <text> Copyright 2014 Vasiliy Vadimov, Nuno Brito </text>
- * FileComment: <text> Testing the algorithm </text> 
+ * FileCopyrightText: <text> Copyright 2015 Nuno Brito </text>
+ * FileComment: <text> Testing the TLSH algorithm </text> 
  */
 
 /**
@@ -21,22 +20,31 @@ import java.io.File;
  */
 public class Test {
 
-    public static void main(String [] args) throws IOException {
+    
+    public static TLSH getData(File file) throws Exception{
+        byte[] tlshData = new byte[(int)file.length()];
+        DataInputStream dataIs = new DataInputStream(new FileInputStream(file));
+        dataIs.readFully(tlshData);
+        TLSH tlsh = new TLSH();
+        tlsh.update(tlshData);
+        tlsh.finale();
+        System.out.println(file.getName() + ": " + tlsh.hash());
+        return tlsh;
+    }
+    
+    public static void main(String [] args) throws Exception {
         
-        TLSH ti1 = new TLSH();
-        File file = new File("favicon.ico");
-	FileInputStream fis = new FileInputStream(file);
- 
-        System.out.println("Total file size to read (in bytes) : "
-                        + fis.available());
-
-        int content;
-        while ((content = fis.read()) != -1) {
-                // convert to char and display it
-                //System.out.print((char) content);
-            //ti1.update((content);
-        }		
+        TLSH t051 = getData(new File("test", "mysql-4051.cc"));
+        TLSH t052 = getData(new File("test", "mysql-4052.cc"));
+        TLSH t059 = getData(new File("test", "mysql-4059.cc"));
+        TLSH t308 = getData(new File("test", "mysql-4308.cc"));
         
+        System.out.println("059 vs Vuln. code: "  + t059.totalDiff(t051, false));
+        System.out.println("059 vs Fixed code: "  + t059.totalDiff(t052, false));
+        System.out.println("308 vs Vuln. code: "  + t308.totalDiff(t051, false));
+        System.out.println("308 vs Fixed code: "  + t308.totalDiff(t052, false));
+        
+        System.exit(0);
     }
 	
     
